78eb2f8c0b2f6ca7a868a9578ba3f3813c19861c
TheIchigoSimp <ypadwal10@gmail.com>
2025-12-09 17:49:31 +0530

    Adding prod url to backend 2

--- Backend/src/lib/redisClient.js
+++ Backend/src/lib/redisClient.js
@@ -1,13 +1,15 @@
-import Redis from "ioredis";
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
 
-const redisClient = new Redis(process.env.REDIS_URL);
+dotenv.config();
 
-redisClient.on("connect", () => {
-  console.log("Connected to Render Redis!");
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
 });
 
-redisClient.on("error", (err) => {
-  console.error("Redis error:", err);
-});
+redisClient.on('error', (err) => console.error('Redis Error ❌', err));
 
-export default redisClient
+await redisClient.connect();
+console.log('Redis connected ✅');
+
+export default redisClient;
