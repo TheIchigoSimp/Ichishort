a94c6c564e2f657e496c520028b1ac9d4da79737
TheIchigoSimp <ypadwal10@gmail.com>
2025-12-09 16:01:29 +0530

    Fixing railway access for vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  cacheDir: '.vite-cache'
+
+  server: {
+    allowedHosts: [
+      'keen-smile-production.up.railway.app',
+    ],
+  },
+
+  preview: {
+    allowedHosts: [
+      'keen-smile-production.up.railway.app',
+    ],
+  },
 })
